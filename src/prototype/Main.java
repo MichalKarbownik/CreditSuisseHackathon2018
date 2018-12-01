8b04abd0a3c7560fde67c3a648790536236ff17b
Michal Karbownik <michakarbownik@gmail.com>
2018-12-01 04:05:19 +0100

    Fixed displaying basic front

--- src/prototype/Main.java
+++ src/prototype/Main.java
@@ -9,10 +9,11 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Prototype.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        primaryStage.setTitle("FXML Welcome");
+        primaryStage.setScene(new Scene(root, 1280 , 720));
         primaryStage.show();
     }
 
